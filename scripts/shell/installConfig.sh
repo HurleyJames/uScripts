0122bcf40329e7573182649e5b4a2f441daa623a
Hurley <1401682479@qq.com>
2021-01-07 21:44:44 +0800

    添加“自动下载必装软件”的脚本

---
+++ scripts/shell/installConfig.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+system_type=$(uname -s)
+
+is_command() {
+	command -v "$1" > /dev/null 2>&1
+}
+
+# macOS
+if [ "$system_type" = "Darwin" ]; then
+	# install zsh
+	if ! is_command zsh; then
+		echo "Installing zsh..."
+		brew install zsh
+		if [ $? -eq 0 ]; then
+			echo "All packages are installed."
+		else
+			echo "Install zsh error."
+		fi
+	else
+		echo "zsh is already existed."
+	fi
+fi
